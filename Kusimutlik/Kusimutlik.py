c828f28d2b52188e10cc1a46059e6d39f8b7dec9
polinagussarova-tech <polina.gussarova@gmail.com>
2026-01-16 23:36:15 +0300

    Refactor user input handling and test logic

--- Kusimutlik/Kusimutlik.py
+++ Kusimutlik/Kusimutlik.py
@@ -1,1 +1,31 @@
+1
+sõnastik, loend=andmete_lugemine_failidest(loend,sõnastik)
+print(sõnastik)
+print(loend)
 
+sõnastik={"A":"a"}
+loend=["A"]
+
+2
+    while True:
+        nimi=input("Sisesta oma nimi: ")
+        N=5
+        if nimi not in nimed:
+            print(F"Tere, {nimi}! Test koosneb {str(N)}küsimusest.")
+            nimed.append(nimi)
+            break
+        else:
+            print("Seda kasutaja on juba testinud!")
+
+4=3 (mi iz tretego zadaniya vinosim 4)
+M = 3
+testitud = []
+
+for _ in range(M):
+    nimi = input("Sisesta nimi: ")
+
+    if nimi in testitud:
+        print("See kasutaja on juba testitud!")
+        continue
+
+    testitud.append(nimi)
