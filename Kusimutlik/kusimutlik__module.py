b2b05d7c00cb717a250697455ce02ed3cd76fb53
Polinnk <Polinnk@PALLONTRASSA>
2026-01-16 19:53:46 +0300

    veel ei tehtud

--- Kusimutlik/kusimutlik__module.py
+++ Kusimutlik/kusimutlik__module.py
@@ -1,1 +1,35 @@
 
+read = []
+kus_vas = {}
+def andmete_lugemine_failidest(read,kus_vas):
+    with open("kysimused_vastused.txt", "r") as f:
+        
+        for rida in f:
+            read.append(rida)
+        
+    
+    for rida in read:
+        kysimus, vastus = rida.split(':')
+        kus_vas[kysimus.strip()] = vastus.strip()
+    return kus_vas, read
+
+sõnastik = {"A":"a"}
+loend = ["A"]
+
+sõnastik, loend = andmete_lugemine_failidest(loend,sõnastik)
+print(sõnastik)
+print(loend)
+
+
+def andmete_salvestamine_failidesse():
+    pass
+
+def testimine():
+    pass
+
+def emaili_saatmine():
+    pass
+
+def küsimuste_lisamine():
+    pass
+
