5db743d67c59e86a8dd5827f51569aee2e9e4db4
polinagussarova-tech <polina.gussarova@gmail.com>
2026-01-17 20:27:03 +0300

    veel ei ole tehtud
    Refactor Kusimutlik.py to include new features such as email sending, question addition, and improved user interaction.

--- Kusimutlik/Kusimutlik.py
+++ Kusimutlik/Kusimutlik.py
@@ -1,31 +1,47 @@
-1
-sõnastik, loend=andmete_lugemine_failidest(loend,sõnastik)
-print(sõnastik)
-print(loend)
+from kusimutlik__module import *
+import random
 
-sõnastik={"A":"a"}
-loend=["A"]
+N=3
+M=2
 
-2
-    while True:
+koik=[]
+oiged=[]
+valed=[]
+juba_testitud=["Mari", "Paul", "Johan"]
+
+read=[]
+kus_vas={}
+kus_vas, read=andmete_lugemine_failidest(read, kus_vas)
+
+valik=input("Vali 1- Alusta küsimustlikku, 2- lisa uus küsimus, 3- välju.)
+if valik=="1":
+
+#2 Küsimustiku läbiviimine ja Vastajate hulk
+    for i in range(M):
         nimi=input("Sisesta oma nimi: ")
-        N=5
-        if nimi not in nimed:
-            print(F"Tere, {nimi}! Test koosneb {str(N)}küsimusest.")
-            nimed.append(nimi)
+    
+        if nimi in juba_testitud:
+            print("Seda kasutaja on juba testitud!")
             break
         else:
-            print("Seda kasutaja on juba testinud!")
+            print(f"Tere, {nimi}!")
+            testitud.append(nimi)
+            vastused = []
 
-4=3 (mi iz tretego zadaniya vinosim 4)
-M = 3
-testitud = []
+#3 Failide salvestamine
+    andmete_salvestamine_failidesse(koik, oiged, valed)
+    print("Failid õiged.txt, valed.txt ja kõik.txt on salvestatud.")
 
-for _ in range(M):
-    nimi = input("Sisesta nimi: ")
+#4 E-kirjade saatmine
+    emaili_saatmine(nimi, punktid, sobis)
+    print("Emaili tulemused on saadetud.")
 
-    if nimi in testitud:
-        print("See kasutaja on juba testitud!")
-        continue
+#5 Võimalus lisada küsimusi programmi kaudu
+if valik=="2":
+    lisada_kusimus=input("Sisesta uus küsimus: ")
+    
+#6 Ekraaniväljund töö lõpus
 
-    testitud.append(nimi)
+#välju kui valik on 3
+if valik=="3":
+    print("Head aega!)
