261abd7c6711f8e124e4448932149c4f8ff3c3f6
polinagussarova-tech <polina.gussarova@gmail.com>
2026-01-17 22:29:12 +0300

    tehtud
    Refactor user input handling and improve feedback for tested users. Added functionality for saving results and allowing new questions to be added.

--- Kusimutlik/Kusimutlik.py
+++ Kusimutlik/Kusimutlik.py
@@ -7,26 +7,38 @@ M=2
 koik=[]
 oiged=[]
 valed=[]
+
+teeb_test=[]
 juba_testitud=["Mari", "Paul", "Johan"]
+
+valik=input("Vali 1- Alusta küsimustlikku, 2- lisa uus küsimus, 3- välju.")
 
 read=[]
 kus_vas={}
 kus_vas, read=andmete_lugemine_failidest(read, kus_vas)
 
-valik=input("Vali 1- Alusta küsimustlikku, 2- lisa uus küsimus, 3- välju.)
 if valik=="1":
 
 #2 Küsimustiku läbiviimine ja Vastajate hulk
     for i in range(M):
-        nimi=input("Sisesta oma nimi: ")
+        while True:
+            nimi=input("Sisesta oma nimi: ")
     
-        if nimi in juba_testitud:
-            print("Seda kasutaja on juba testitud!")
-            break
+            if nimi in juba_testitud:
+                print("See kasutaja on juba testitud! Sisesta teist nime.")
+            else:
+                print(f"Tere, {nimi}!")
+                teeb_test.append(nimi)
+                break
+#testimine
+        punktid, sobis=testimine(kus_vas, N, nimi)
+        email=nimi.replace(" ", ".")+"@example.com"
+        koik.append([nimi, punktid, email])
+
+        if sobis:
+            oiged.append([nimi, punktid])
         else:
-            print(f"Tere, {nimi}!")
-            testitud.append(nimi)
-            vastused = []
+            valed.append([nimi, punktid])
 
 #3 Failide salvestamine
     andmete_salvestamine_failidesse(koik, oiged, valed)
@@ -38,10 +50,19 @@ if valik=="1":
 
 #5 Võimalus lisada küsimusi programmi kaudu
 if valik=="2":
-    lisada_kusimus=input("Sisesta uus küsimus: ")
+    kusimus=input("Sisesta uus küsimus: ")
+    vastus=input("Sisesta õige vastus: ")
+
+    küsimuste_lisamine(kusimus, vastus)
+    print("Uus küsimus on lisatud faili.")
     
 #6 Ekraaniväljund töö lõpus
+    print("Edukalt vastanud:")
+    for inimene in oiged:
+        print(inimene[0], "-", inimene[1], "õigesti")
 
+    print("Tulemused saadetud e-posti aadressidele.")
+    
 #välju kui valik on 3
 if valik=="3":
-    print("Head aega!)
+    print("Head aega!")
