c6acbcf284701823b5ab968a48c3f132b07dd906
polinagussarova-tech <polina.gussarova@gmail.com>
2026-01-18 17:07:24 +0300

    woww

--- Kusimutlik/kusimutlik__module.py
+++ Kusimutlik/kusimutlik__module.py
@@ -1,5 +1,4 @@
 import random
-
 kus_vas={} #sõnastik, kus võtmed on küsimused ja väärtused vastused
 
 def andmete_lugemine_failidest(kus_vas):    
@@ -24,42 +23,42 @@ def testimine(kus_vas,N, nimi):
 
 
 def andmete_salvestamine_failidesse(koik, oiged, valed):
-    oiged.sort(key=lambda x: x[1], reverse=True)
-    valed.sort(key=lambda x: x[0])
-    
-    with open("õiged.txt", "w", encoding="utf-8") as f:
+    oiged.sort(key=lambda inimene: inimene["punktid"], reverse=True)
+    valed.sort(key=lambda inimene: inimene["nimi"])
+
+    with open("oiged.txt", "w", encoding="utf-8") as f:
         for inimene in oiged:
-            nimi=inimene[0]
-            punktid=inimene[1]
-            print(f"{nimi} - {punktid} õigesti", file=f)
-    
+            print(inimene["nimi"], "-", inimene["punktid"], "oigesti", file=f)
+
     with open("valed.txt", "w", encoding="utf-8") as f:
         for inimene in valed:
-            nimi=inimene[0]
-            punktid=inimene[1]
-            print(f"{nimi} - {punktid} valesti", file=f)
+            print(inimene["nimi"], "-", inimene["punktid"], "valesti", file=f)
 
     with open("koik.txt", "w", encoding="utf-8") as f:
         for inimene in koik:
-            nimi=inimene[0]
-            punktid=inimene[1]
-            email=inimene[2]
-            print(nimi, punktid, email, file=f)
+            print(inimene["nimi"], "-", inimene["punktid"], "-", inimene["email"], file=f)
 
+    
 def emaili_saatmine(nimi, punktid, sobis):
-    email=nimi.replace(" ", ".") + "@example.com"
+    email=nimi.lower().replace(" ", ".") + "@example.com"
 
-    print(f"Saadetud {email}")
+    print(f"Saadetud:{email}")
     print(f"Tere, {nimi}!")
-    print(f"Sinu õigete vastuste arv on {punktid}")
+    print(f"Sinu õigete vastuste arv on: {punktid}")
 
     if sobis:
         print("Sa sooritasid testi edukalt.")
     else:
         print("Kahjuks testi ei sooritatud edukalt.")
 
+def raport_tooandjale(koik):
+    print("Saadetud: tootaja@firma.ee")
+    print("Tere!")
+    print("Tänased küsimustiku tulemused:")
+    
+    koik.sort(key=lambda inimene: inimene["punktid"], reverse=True)
+    parim=koik[0]
+
 def küsimuste_lisamine(kusimus, vastus):
     with open("kusimused_vastused.txt", "a", encoding="utf-8") as f:
         print(f"{kusimus}:{vastus}", file=f)
-
-
