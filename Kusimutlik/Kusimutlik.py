647279c6a95eca8f034e97a3c9a5c435cee7bdc8
polinagussarova-tech <polina.gussarova@gmail.com>
2026-01-18 18:19:30 +0300

    tehtud

--- Kusimutlik/Kusimutlik.py
+++ Kusimutlik/Kusimutlik.py
@@ -10,58 +10,64 @@ valed=[]
 
 juba_testitud=["Mari", "Paul", "Johan"]
 
-valik=input("Vali 1- Alusta küsimustlikku, 2- lisa uus küsimus, 3- välju.")
-
-read=[]
 kus_vas={}
-kus_vas, read=andmete_lugemine_failidest(read, kus_vas)
 
-if valik=="1":
-
+while True:
+    valik=input("Vali 1- Alusta küsimustlikku, 2- lisa uus küsimus, 3- välju: ")
+    
 #2 Küsimustiku läbiviimine ja Vastajate hulk
-    for i in range(M):
-        while True:
-            nimi=input("Sisesta oma nimi: ")
+    if valik=="1":
     
-            if nimi in juba_testitud:
-                print("See kasutaja on juba testitud! Sisesta teist nime.")
-            else:
-                print(f"Tere, {nimi}!")
-                juba_testitud.append(nimi)
-                break
-#testimine
-        punktid, sobis=testimine(kus_vas, N, nimi)
-        email=nimi.replace(" ", ".")+"@example.com"
-        koik.append([nimi, punktid, email])
-
-        if sobis:
-            oiged.append([nimi, punktid])
-        else:
-            valed.append([nimi, punktid])
-
-#3 Failide salvestamine
-    andmete_salvestamine_failidesse(koik, oiged, valed)
-    print("Failid õiged.txt, valed.txt ja kõik.txt on salvestatud.")
-
-#4 E-kirjade saatmine
-    emaili_saatmine(nimi, punktid, sobis)
-    print("Emaili tulemused on saadetud.")
-
-#5 Võimalus lisada küsimusi programmi kaudu
-if valik=="2":
-    kusimus=input("Sisesta uus küsimus: ")
-    vastus=input("Sisesta õige vastus: ")
-
-    küsimuste_lisamine(kusimus, vastus)
-    print("Uus küsimus on lisatud faili.")
+        kus_vas=andmete_lugemine_failidest(kus_vas)
+            for i in range(M):
+                while True:
+                    nimi=input("Sisesta oma nimi: ")
+            
+                    if nimi in juba_testitud:
+                        print("See kasutaja on juba testitud! Sisesta teist nime.")
+                    else:
+                        print(f"Tere, {nimi}! Alustame test.")
+                        juba_testitud.append(nimi)
+                        break
+#3 testimine
+                punktid, sobis=testimine(kus_vas, N, nimi)
+                email=nimi.replace(" ", ".")+"@example.com"
+                
+                vastaja={"nimi":nimi, "punktid":punktid, "email":email, "sobis":sobis}
+                koik.append(vastaja)
+        
+                if sobis:
+                    oiged.append(vastaja)
+                else:
+                    valed.append(vastaja)
+        
+#4 Failide salvestamine
+            andmete_salvestamine_failidesse(koik, oiged, valed)
+            print("Failid õiged.txt, valed.txt ja kõik.txt on salvestatud.")
     
-#6 Ekraaniväljund töö lõpus
-    print("Edukalt vastanud:")
-    for inimene in oiged:
-        print(inimene[0], "-", inimene[1], "õigesti")
-
-    print("Tulemused saadetud e-posti aadressidele.")
+#5 E-kirjade saatmine
+        emaili_saatmine(nimi, punktid, sobis)
+        print("Emaili tulemused on saadetud.")
+        
+        raport_tooandjale(koik)
     
-#välju kui valik on 3
-if valik=="3":
-    print("Head aega!")
+#6 Võimalus lisada küsimusi programmi kaudu
+    if valik=="2":
+        kusimus=input("Sisesta uus küsimus: ")
+        vastus=input("Sisesta õige vastus: ")
+    
+        küsimuste_lisamine(kusimus, vastus)
+        print("Uus küsimus on lisatud faili.")
+        
+#7 Ekraaniväljund töö lõpus
+        print("Edukalt vastanud:")
+        for vastaja in oiged:
+            print(vastaja["nimi"], "-", vastaja["punktid"], "õigesti")
+    
+        print("Tulemused saadetud e-posti aadressidele.")
+        
+#8 välju kui valik on 3
+    if valik=="3":
+        print("Head aega!")
+else:
+    print("Sisesta ainult 1, 2 või 3!")
